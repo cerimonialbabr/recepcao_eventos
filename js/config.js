d5190d25dea07b0ecc1f973498bb70347e4e60b1
cerimonialbabr <cerimonial.babr@gmail.com>
2026-07-22 19:09:59 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwk-UKX8r1q1YeUMKrPq77M5Eu4FIL3Ht1F-UJzIOpQtRwyIYL3xBcGPY_Bj_10wMIL/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbychB47rqXpCab-3C2Tjyl7XYiAjK5itU1QkRqcDfRtI0ampneJre-NFvnuLmMFilX-/exec",
   INTERVALO_ATUALIZACAO_MS: 10000,
   TAMANHO_MAXIMO_FOTO_MB: 5
 };
